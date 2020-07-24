c50bd88282009159f7765ef926830d6975d0d53e
masatoshi.tezuka <masted3325@gmail.com>
2020-07-24 09:12:24 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>”いい湯だな～♨”</br>にようこそ</h1>
+            {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route('signup.get', '会員登録', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
